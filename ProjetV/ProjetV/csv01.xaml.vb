604d1d4c9a2e93e685b937360aaa09d7116d02f2
olivier <olivier.jacob@campus-eni.fr>
2017-06-26 16:58:50 +0200

    commit

--- ProjetV/ProjetV/csv01.xaml.vb
+++ ProjetV/ProjetV/csv01.xaml.vb
@@ -1,16 +1,33 @@
 ﻿Imports System.IO
-Imports Microsoft.Win32
-Imports System.Windows.Controls
-Imports System.Windows.Documents
+Imports System.Windows.Forms
+Imports System.Windows
+Imports System
+Imports System.Windows.Input
+Imports System.Windows.Media
+
 
 
 Class csv01
 
 
 
-
     Private Sub rtbEditor_SelectionChanged(ByVal sender As Object, ByVal e As RoutedEventArgs)
 
+        'Dim temp As Object = rtbEditor.Selection.GetPropertyValue(Inline.FontWeightProperty)
+        'btnBold.IsChecked = ((temp <> DependencyProperty.UnsetValue) _
+        'AndAlso temp.Equals(FontWeights.Bold))
+        ' temp = rtbEditor.Selection.GetPropertyValue(Inline.FontStyleProperty)
+        '  btnItalic.IsChecked = ((temp <> DependencyProperty.UnsetValue) _
+        '             AndAlso temp.Equals(FontStyles.Italic))
+        ' temp = rtbEditor.Selection.GetPropertyValue(Inline.TextDecorationsProperty)
+        'btnUnderline.IsChecked = ((temp <> DependencyProperty.UnsetValue) _
+        '         AndAlso temp.Equals(TextDecorations.Underline))
+        'temp = rtbEditor.Selection.GetPropertyValue(Inline.FontFamilyProperty)
+        'cmbFontFamily.SelectedItem = temp
+        'temp = rtbEditor.Selection.GetPropertyValue(Inline.FontSizeProperty)
+        'cmbFontSize.Text = temp.ToString
+
+        '
     End Sub
 
 
@@ -18,12 +35,12 @@ Class csv01
 
     Private Sub btnOpen_Click(ByVal sender As Object, ByVal e As ExecutedRoutedEventArgs) Handles btnOpen.Click
 
-        Dim dlg As OpenFileDialog = New OpenFileDialog
-        dlg.Filter = "Comma separated values (*.csv)|*.csv|All files (*.*)|*.*"
+        Dim dlg As Microsoft.Win32.OpenFileDialog = New Microsoft.Win32.OpenFileDialog
+
         If (dlg.ShowDialog = True) Then
             Dim fileStream As FileStream = New FileStream(dlg.FileName, FileMode.Open)
             Dim range As TextRange = New TextRange(rtbEditor.Document.ContentStart, rtbEditor.Document.ContentEnd)
-            range.Load(fileStream, DataFormats.CommaSeparatedValue)
+            range.Load(fileStream, Windows.DataFormats.CommaSeparatedValue)
 
         End If
 
@@ -31,13 +48,15 @@ Class csv01
 
     End Sub
 
+
+
     Private Sub btnSave_Click(ByVal sender As Object, ByVal e As ExecutedRoutedEventArgs) Handles btnSave.Click
-        Dim dlg As SaveFileDialog = New SaveFileDialog
+        Dim dlg As Microsoft.Win32.SaveFileDialog = New Microsoft.Win32.SaveFileDialog
         dlg.Filter = "Comma separated values (*.csv)|*.csv|All files (*.*)|*.*"
         If (dlg.ShowDialog = True) Then
             Dim fileStream As FileStream = New FileStream(dlg.FileName, FileMode.Create)
             Dim range As TextRange = New TextRange(rtbEditor.Document.ContentStart, rtbEditor.Document.ContentEnd)
-            range.Save(fileStream, DataFormats.CommaSeparatedValue)
+            range.Save(fileStream, Windows.DataFormats.CommaSeparatedValue)
         End If
 
     End Sub
@@ -69,19 +88,23 @@ Class csv01
     End Sub
 
     Private Sub btnGetText_Click(sender As Object, e As RoutedEventArgs)
-        Throw New NotImplementedException()
+        Dim textRange As TextRange = New TextRange(rtbEditor.Document.ContentStart, rtbEditor.Document.ContentEnd)
+        Windows.MessageBox.Show(textRange.Text)
     End Sub
 
     Private Sub btnSetText_Click(sender As Object, e As RoutedEventArgs)
-        Throw New NotImplementedException()
+        Dim textRange As TextRange = New TextRange(rtbEditor.Document.ContentStart, rtbEditor.Document.ContentEnd)
+        textRange.Text = "Another world, another text!"
     End Sub
 
     Private Sub btnGetSelectedText_Click(sender As Object, e As RoutedEventArgs)
-        Throw New NotImplementedException()
+        Windows.MessageBox.Show(rtbEditor.Selection.Text)
     End Sub
 
     Private Sub btnSetSelectedText_Click(sender As Object, e As RoutedEventArgs)
-        Throw New NotImplementedException()
+        rtbEditor.Selection.Text = "[Replaced text]"
     End Sub
+
+
 End Class
 
