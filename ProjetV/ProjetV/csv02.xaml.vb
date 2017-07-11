3336973e7376861dc237cb8ee167500d0cfb2333
olivier <olivier.jacob@campus-eni.fr>
2017-07-11 16:13:00 +0200

    update

--- ProjetV/ProjetV/csv02.xaml.vb
+++ ProjetV/ProjetV/csv02.xaml.vb
@@ -1,18 +1,42 @@
 ﻿
+Imports System.IO
+Imports System.Text
+Imports System.Windows.Forms
+Imports Microsoft.Win32
+Imports System.Management.Automation
+Imports System.Management.Automation.Runspaces
+Imports System.Collections.ObjectModel
 
 Public Class csv02
 
+    'Sub View(send As Object, e As EventArgs)
+    'Using ps As PowerShell = PowerShell.Create()
+    'Dim logFilePathName As String = "C:\Users\Olivier  - SP3\Documents\Visual Studio 2015\ProjetENI2\export\"
+    '       ps.AddScript(String.Format("get-content {0} -wait", logFilePathName))
+    'Dim outputCollection As PSDataCollection(Of PSObject) = New PSDataCollection(Of PSObject)
+    'AddHandler() outputCollection.DataAdded, AddressOf LogFileOutput
+    'Dim result As IAsyncResult = ps.BeginInvoke(Of PSObject, PSObject)(Nothing, outputCollection)
+    'End Using
+    'End Sub
+
+    'Sub LogFileOutput(send As Object, data As DataAddedEventArgs)
+    '   ListBox1 = data.ToString
+    'End Sub
+
     Private Sub buttoncsv22_Click(sender As Object, e As RoutedEventArgs) Handles buttoncsv22.Click
-        Dim csv03 As New csv03
-        Hide()
-        csv03.Show
-    End Sub
+            Dim csv03 As New csv03
+            Hide()
+            csv03.Show()
+        End Sub
 
-    Private Sub buttoncsv23_Click(sender As Object, e As RoutedEventArgs) Handles buttoncsv23.Click
+        Private Sub buttoncsv23_Click(sender As Object, e As RoutedEventArgs) Handles buttoncsv23.Click
 
-        '   Dim Textbox1 As New TextBox
-        '  Textbox1 = ListBox1.SelectedItems
 
+        'Dim textbox1 As New TextBox
+        'textbox1.Clear()
+
+        'ListBox1.SelectedItems 
+        'ListBox2.SelectedItems
 
 
     End Sub
---
+++ ProjetV/ProjetV/csv03.xaml.vb
@@ -0,0 +1,31 @@
+﻿
+
+Public Class csv03
+
+
+    'Private Sub Report(ByVal sender As Object, ByVal e As EventArgs)
+    '   Dim Select = "SELECT ID,nom FROM SITE INNER JOIN ID,nom from FORMATION"
+    'Dim c = New
+    '   SqlConnection(yourConnectionString) ' Connection String here
+    'Dim dataAdapter = New
+    '   SqlDataAdapter(select, c)
+    'Dim commandBuilder = New
+    '   SqlCommandBuilder(dataAdapter)
+    'Dim ds = New
+    '   DataSet
+    '  dataAdapter.Fill(ds)
+    ' dataGrid.IsReadOnly = True
+    'dataGrid.DataContext = ds.Tables(0)
+    'End Sub
+
+
+    Private Sub buttoncsv21_Click(sender As Object, e As RoutedEventArgs) Handles buttoncsv21.Click
+
+    End Sub
+
+    Private Sub buttoncsv22_Click(sender As Object, e As RoutedEventArgs) Handles buttoncsv22.Click
+
+    End Sub
+End Class
+
+
